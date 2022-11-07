37cb1acd510ca20224a410e2007769ae461a0632
lichunyu <lichunyu0410@qq.com>
2022-11-07 23:45:52 +0800

    auto-pair

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -71,6 +71,8 @@ return packer.startup(function(use)
   use "williamboman/mason.nvim" -- simple to use language server installer
   use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
 
+  use "windwp/nvim-autopairs"
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
